bafbd4dd5a408b39d78c8e763aafe73e58cc44b4
Ali Alanazi <76615100+ali3314@users.noreply.github.com>
2022-08-04 13:34:05 +0300

    Add files via upload

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,18 +13,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val txt_number : TextView = findViewById(R.id.rolltext)
-        val btnroll : Button = findViewById(R.id.butroll)
+        val rollButton : Button = findViewById(R.id.butroll)
 
-        btnroll.setOnClickListener {
-
-            // declare variable for toast
-            val toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT)
-
-            // function for show toast
-            toast.show()
+        rollButton.setOnClickListener {
+            rollDice()
         }
-
     }
+
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // create object
+        val dice = Dice(6)
+        // save the result number
+        val diceRoll = dice.roll()
+
+        // reference to imageView
+        val diceImage : ImageView = findViewById(R.id.imageView)
+
+        // check number and update screen (UI)
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        } // End when
+    } // End rollDice()
 
 }
